b10f255c2863e06c2b3315ecc0f6f65a17e0cdc0
Michael Peacock <mkpeacock@gmail.com>
2016-05-11 19:22:13 +0100

    Sample implementation of the dependency configuration
    Used for callable resolutions

---
+++ src/DependencyConfigurations/CallableResolution.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ParityBit\DependencyResolver\DependencyConfigurations;
+
+class CallableResolution implements Resolution
+{
+    protected $callable;
+
+    public function __construct(callable $callable)
+    {
+        $this->callable = $callable;
+    }
+
+    public function getDependency()
+    {
+        return call_user_func($this->callable)
+    }
+}
